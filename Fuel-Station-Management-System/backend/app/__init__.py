af376d8f9d292b5d4361e5242be7c8af5720c42e
SenithDamiru <senithdamiru2005@gmail.com>
2025-06-27 23:30:17 +0530

    shits and expenses

--- Fuel-Station-Management-System/backend/app/__init__.py
+++ Fuel-Station-Management-System/backend/app/__init__.py
@@ -13,6 +13,17 @@ def create_app():
     from .routes.role_routes import role_bp
     app.register_blueprint(role_bp)
 
+    from .routes.staff_routes import staff_bp
+    app.register_blueprint(staff_bp)
+
+    from .routes.shift_routes import shift_bp  
+    app.register_blueprint(shift_bp)
+
+    from .routes.expenses_routes import expenses_bp
+    app.register_blueprint(expenses_bp)
+
+
+
     from flask import render_template
     @app.route('/')
     def index():
